d6a0e6b47a74c2c7e7905bc63612b29b61c17daf
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-03-30 23:53:05 +0200

    Introduce updateBackstagePass

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -22,28 +22,31 @@ class GildedRose(val items: Array[Item]) {
 
   }
 
+  private def updateBackstagePass(item: Item): Unit = {
+    item.sellIn -= 1
+
+    // This is necessary to preserve (bad) legacy handling of sell-in underflowing ints:
+    // - before update of sell-in, it's < 6, so quality is incremented by 3
+    // - after update, it's greater than 0, so its quality is not reset.
+    if(item.sellIn == Int.MaxValue)
+      increaseQuality(item, 3)
+    else if(item.sellIn < 0)
+      item.quality = 0
+    else if(item.sellIn < 5)
+      increaseQuality(item, 3)
+    else if(item.sellIn < 10)
+      increaseQuality(item, 2)
+    else
+      increaseQuality(item, 1)
+
+  }
+
   def updateQuality() {
     items.foreach { item =>
       if(isBrie(item))
         updateBrie(item)
-      else if(isBackstagePass(item)) {
-        item.sellIn -= 1
-
-        // This is necessary to preserve (bad) legacy handling of sell-in underflowing ints:
-        // - before update of sell-in, it's < 6, so quality is incremented by 3
-        // - after update, it's greater than 0, so its quality is not reset.
-        if(item.sellIn == Int.MaxValue)
-          increaseQuality(item, 3)
-        else if(item.sellIn < 0)
-          item.quality = 0
-        else if(item.sellIn < 5)
-          increaseQuality(item, 3)
-        else if(item.sellIn < 10)
-          increaseQuality(item, 2)
-        else
-          increaseQuality(item, 1)
-
-      }
+      else if(isBackstagePass(item))
+        updateBackstagePass(item)
       else if(!isSulfuras(item)) {
         item.sellIn -= 1
         if(item.sellIn < 0)
