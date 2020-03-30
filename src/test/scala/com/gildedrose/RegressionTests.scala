916dce8f06fbe0917ac075c36ad01798131bf088
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-03-30 17:55:20 +0200

    Clone test inventories
    We now have another issue linked to Java being messed up: equal items
    are not equal in the Scala sense. To actually be equal, they must be
    the same reference, which is not the case anymore here.
    
    This is something we have to fix in two ways:
    - use a sane equality
    - make sure we don't lose that interesting invariant that crazy people
      might actually rely on

--- src/test/scala/com/gildedrose/RegressionTests.scala
+++ src/test/scala/com/gildedrose/RegressionTests.scala
@@ -4,11 +4,15 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class RegressionTests extends AnyFunSuite with Matchers {
+
+  def cloneItems(inventory: Array[Item]): Array[Item] =
+    inventory.map(item => new Item(item.name, item.sellIn, item.quality))
+
   test("Legacy and modern implementations should behave the same") {
     val inventory = Array(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20))
 
-    val legacy = new LegacyGildedRose(inventory)
-    val modern = new GildedRose(inventory)
+    val legacy = new LegacyGildedRose(cloneItems(inventory))
+    val modern = new GildedRose(cloneItems(inventory))
 
     legacy.updateQuality()
     modern.updateQuality()
