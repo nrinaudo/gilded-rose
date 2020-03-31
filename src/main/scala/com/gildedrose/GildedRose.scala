0fda9fe5064c4b364f1c25288abcbe9a7522efb8
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-03-31 22:40:50 +0200

    Make updateQuality use fp.Item

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -2,62 +2,66 @@ package com.gildedrose
 
 class GildedRose(val items: Array[Item]) {
 
-  private def isBrie(item: Item): Boolean          = item.name.equals("Aged Brie")
-  private def isBackstagePass(item: Item): Boolean = item.name.equals("Backstage passes to a TAFKAL80ETC concert")
-  private def isSulfuras(item: Item): Boolean      = item.name.equals("Sulfuras, Hand of Ragnaros")
+  private def isBrie(item: fp.Item): Boolean          = item.name.equals("Aged Brie")
+  private def isBackstagePass(item: fp.Item): Boolean = item.name.equals("Backstage passes to a TAFKAL80ETC concert")
+  private def isSulfuras(item: fp.Item): Boolean      = item.name.equals("Sulfuras, Hand of Ragnaros")
 
   private def increaseQuality(quality: Int, quantity: Int): Int =
     if(quality < 50) math.min(50, quantity + quality)
     else quality
 
-  private def updateBrie(item: Item): Item = {
-    item.sellIn -= 1
+  private def updateBrie(item: fp.Item): fp.Item = {
+    val sellIn   = item.sellIn - 1
+    val quantity = if(sellIn < 0) 2 else 1
 
-    if(item.sellIn < 0)
-      item.quality = increaseQuality(item.quality, 2)
-    else
-      item.quality = increaseQuality(item.quality, 1)
-
-    item
+    item.copy(
+      sellIn = sellIn,
+      quality = increaseQuality(item.quality, quantity)
+    )
   }
 
-  private def updateBackstagePass(item: Item): Item = {
-    item.sellIn -= 1
+  private def updateBackstagePass(item: fp.Item): fp.Item = {
+    val sellIn = item.sellIn - 1
 
-    // This is necessary to preserve (bad) legacy handling of sell-in underflowing ints:
-    // - before update of sell-in, it's < 6, so quality is incremented by 3
-    // - after update, it's greater than 0, so its quality is not reset.
-    if(item.sellIn == Int.MaxValue)
-      item.quality = increaseQuality(item.quality, 3)
-    else if(item.sellIn < 0)
-      item.quality = 0
-    else if(item.sellIn < 5)
-      item.quality = increaseQuality(item.quality, 3)
-    else if(item.sellIn < 10)
-      item.quality = increaseQuality(item.quality, 2)
-    else
-      item.quality = increaseQuality(item.quality, 1)
+    if(sellIn < 0)
+      item.copy(
+        sellIn = sellIn,
+        quality = 0
+      )
+    else {
+      // This is necessary to preserve (bad) legacy handling of sell-in underflowing ints:
+      // - before update of sell-in, it's < 6, so quality is incremented by 3
+      // - after update, it's greater than 0, so its quality is not reset.
+      val quantity =
+        if(sellIn == Int.MaxValue || sellIn < 5) 3
+        else if(sellIn < 10) 2
+        else 1
 
-    item
+      item.copy(
+        sellIn = sellIn,
+        quality = increaseQuality(item.quality, quantity)
+      )
+    }
   }
 
-  private def updateSulfuras(item: Item): Item = item
+  private def updateSulfuras(item: fp.Item): fp.Item = item
 
-  private def updateRegularItem(item: Item): Item = {
+  private def updateRegularItem(item: fp.Item): fp.Item = {
 
-    def decreaseQuality(quantity: Int): Unit =
-      if(item.quality > 0) item.quality = math.max(0, item.quality - quantity)
+    def decreaseQuality(quantity: Int): Int =
+      if(item.quality > 0) math.max(0, item.quality - quantity)
+      else item.quality
 
-    item.sellIn -= 1
-    if(item.sellIn < 0)
-      decreaseQuality(2)
-    else
-      decreaseQuality(1)
+    val sellIn   = item.sellIn - 1
+    val quantity = if(sellIn < 0) 2 else 1
 
-    item
+    item.copy(
+      sellIn = sellIn,
+      quality = decreaseQuality(quantity)
+    )
   }
 
-  private def updateItem(item: Item): Item =
+  private def updateItem(item: fp.Item): fp.Item =
     if(isBrie(item))
       updateBrie(item)
     else if(isBackstagePass(item))
@@ -68,6 +72,10 @@ class GildedRose(val items: Array[Item]) {
       updateRegularItem(item)
 
   def updateQuality(): Unit =
-    items.foreach(updateItem)
+    items.foreach { item =>
+      val updated = updateItem(fp.Item(item.name, item.sellIn, item.quality))
 
+      item.sellIn = updated.sellIn
+      item.quality = updated.quality
+    }
 }
