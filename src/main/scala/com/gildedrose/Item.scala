994adb7d4d4ac3ed8df00dbb9acc0c9f58c36461
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-04-01 14:49:06 +0200

    Deprecate com.gildedrose.Item

--- src/main/scala/com/gildedrose/Item.scala
+++ src/main/scala/com/gildedrose/Item.scala
@@ -1,5 +1,6 @@
 package com.gildedrose
 
+@deprecated("this class will be removed use com.gildedrose.fp.Item instead", "1.1")
 class Item(val name: String, var sellIn: Int, var quality: Int) {
   override def toString = s"Item($name, $sellIn, $quality)"
 }
