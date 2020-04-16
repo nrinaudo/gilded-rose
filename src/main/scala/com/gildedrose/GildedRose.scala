997a7a2b7b450a0ea36c410803e833bb65003971
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-04-16 23:25:32 +0200

    Make increaseQuality take a quantity argument

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -6,8 +6,8 @@ class GildedRose(val items: Array[Item]) {
   private def isPass(item: Item): Boolean     = item.name.equals("Backstage passes to a TAFKAL80ETC concert")
   private def isSulfuras(item: Item): Boolean = item.name.equals("Sulfuras, Hand of Ragnaros")
 
-  private def increaseQuality(item: Item) =
-    if(item.quality < 50) item.quality = item.quality + 1
+  private def increaseQuality(item: Item, quantity: Int) =
+    if(item.quality < 50) item.quality = math.min(item.quality + quantity, 50)
 
   private def decreaseQuality(item: Item) =
     if(item.quality > 0 && !isSulfuras(item)) item.quality = item.quality - 1
@@ -25,23 +25,22 @@ class GildedRose(val items: Array[Item]) {
       val expired = item.sellIn < 0
 
       if(isBrie(item)) {
-        increaseQuality(item)
+        increaseQuality(item, 1)
         if(expired)
-          increaseQuality(item)
+          increaseQuality(item, 1)
       }
       else if(isPass(item)) {
         if(expired)
           item.quality = 0
         else {
-
-          increaseQuality(item)
+          increaseQuality(item, 1)
 
           if(late(item.sellIn)) {
-            increaseQuality(item)
+            increaseQuality(item, 1)
           }
 
           if(lastMinute(item.sellIn)) {
-            increaseQuality(item)
+            increaseQuality(item, 1)
           }
         }
       }
