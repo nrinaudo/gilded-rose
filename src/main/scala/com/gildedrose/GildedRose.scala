5a4fed3f9e9c7be7338801a9a3b29b16b01da2cc
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-04-16 23:27:14 +0200

    Aggregate increaseQuality calls

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -14,7 +14,8 @@ class GildedRose(val items: Array[Item]) {
 
   private def lastMinute(sellIn: Int) = sellIn < 5 || sellIn == Int.MaxValue
 
-  private def late(sellIn: Int)       = sellIn < 10 || sellIn == Int.MaxValue
+  private def late(sellIn: Int) = sellIn < 10
+
   private def decreaseSellIn(item: Item) =
     if(!isSulfuras(item)) item.sellIn = item.sellIn - 1
 
@@ -25,23 +26,21 @@ class GildedRose(val items: Array[Item]) {
       val expired = item.sellIn < 0
 
       if(isBrie(item)) {
-        increaseQuality(item, 1)
         if(expired)
+          increaseQuality(item, 2)
+        else
           increaseQuality(item, 1)
       }
       else if(isPass(item)) {
         if(expired)
           item.quality = 0
         else {
-          increaseQuality(item, 1)
-
-          if(late(item.sellIn)) {
+          if(lastMinute(item.sellIn))
+            increaseQuality(item, 3)
+          else if(late(item.sellIn))
+            increaseQuality(item, 2)
+          else
             increaseQuality(item, 1)
-          }
-
-          if(lastMinute(item.sellIn)) {
-            increaseQuality(item, 1)
-          }
         }
       }
       else {
