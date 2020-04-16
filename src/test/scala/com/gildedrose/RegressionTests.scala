0eb3126bd84348cd8e441114b010e549270586c6
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-04-16 22:54:05 +0200

    Add simple non-regression test
    This is not yet very interesting though - we're testing a single
    scenario. It would be better to have random inputs.

---
+++ src/test/scala/com/gildedrose/RegressionTests.scala
@@ -0,0 +1,25 @@
+package com.gildedrose
+
+import org.scalacheck.Gen
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+
+class RegressionTests extends AnyFunSuite with Matchers with ScalaCheckDrivenPropertyChecks {
+  test("The legacy implementation and the modernized version should be equivalent") {
+
+    val name    = "Foobar"
+    val quality = 10
+    val sellIn  = 9
+
+    val legacy = new Item(name, sellIn, quality)
+    new LegacyGildedRose(Array(legacy)).updateQuality()
+
+    val modern = new Item(name, sellIn, quality)
+    new GildedRose(Array(modern)).updateQuality()
+
+    legacy.name should be(modern.name)
+    legacy.quality should be(modern.quality)
+    legacy.sellIn should be(modern.sellIn)
+  }
+}
