b0c7d3e873eeb07beec01bedc1d0bfce228ff85c
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-04-17 17:36:30 +0200

    Add tests for non-expired conjured items

---
+++ src/test/scala/com/gildedrose/GildedRoseTests.scala
@@ -0,0 +1,17 @@
+package com.gildedrose
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+
+class GildedroseTests extends AnyFunSuite with Matchers with ScalaCheckDrivenPropertyChecks {
+
+  test("Non-expired conjured items should degrade at the rate of 2 every day") {
+    val item = new Item("Conjured item", 10, 2)
+
+    new GildedRose(Array(item)).updateQuality()
+
+    item.quality should be(0)
+  }
+
+}
