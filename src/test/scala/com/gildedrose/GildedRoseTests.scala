d196843245ebbe8039fe48e571c99e75fd395b3d
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-04-19 19:20:44 +0200

    Make conjured item tests vary on quality
    Note that we focus on valid quality values here. Anything else is unspecified.

--- src/test/scala/com/gildedrose/GildedRoseTests.scala
+++ src/test/scala/com/gildedrose/GildedRoseTests.scala
@@ -9,24 +9,25 @@ import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 class GildedroseTests extends AnyFunSuite with Matchers with ScalaCheckDrivenPropertyChecks {
 
   val conjuredNameGen: Gen[String] = arbitrary[String].map(name => s"Conjured $name")
+  val qualityGen: Gen[Int]         = Gen.chooseNum(0, 50)
 
   test("Non-expired conjured items should degrade at the rate of 2 every day") {
-    forAll(conjuredNameGen -> "name") { name =>
-      val item = new Item(name, 10, 2)
+    forAll(conjuredNameGen -> "name", qualityGen -> "quality") { (name, quality) =>
+      val item = new Item(name, 10, quality)
 
       new GildedRose(Array(item)).updateQuality()
 
-      item.quality should be(0)
+      item.quality should be(math.max(0, quality - 2))
     }
   }
 
   test("Expired conjured items should degrade at the rate of 4 every day") {
-    forAll(conjuredNameGen -> "name") { name =>
-      val item = new Item(name, 0, 5)
+    forAll(conjuredNameGen -> "name", qualityGen -> "quality") { (name, quality) =>
+      val item = new Item(name, 0, quality)
 
       new GildedRose(Array(item)).updateQuality()
 
-      item.quality should be(1)
+      item.quality should be(math.max(0, quality - 4))
     }
   }
 
