d6b6494da80d6c8b5bb20f155b9bc26c2939a03f
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-04-19 19:19:22 +0200

    Make conjured item tests vary on the item's name

--- src/test/scala/com/gildedrose/GildedRoseTests.scala
+++ src/test/scala/com/gildedrose/GildedRoseTests.scala
@@ -1,25 +1,33 @@
 package com.gildedrose
 
+import org.scalacheck.{Arbitrary, Gen}
+import Arbitrary.arbitrary
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
 class GildedroseTests extends AnyFunSuite with Matchers with ScalaCheckDrivenPropertyChecks {
 
+  val conjuredNameGen: Gen[String] = arbitrary[String].map(name => s"Conjured $name")
+
   test("Non-expired conjured items should degrade at the rate of 2 every day") {
-    val item = new Item("Conjured item", 10, 2)
+    forAll(conjuredNameGen -> "name") { name =>
+      val item = new Item(name, 10, 2)
 
-    new GildedRose(Array(item)).updateQuality()
+      new GildedRose(Array(item)).updateQuality()
 
-    item.quality should be(0)
+      item.quality should be(0)
+    }
   }
 
   test("Expired conjured items should degrade at the rate of 4 every day") {
-    val item = new Item("Conjured item", 0, 5)
+    forAll(conjuredNameGen -> "name") { name =>
+      val item = new Item(name, 0, 5)
 
-    new GildedRose(Array(item)).updateQuality()
+      new GildedRose(Array(item)).updateQuality()
 
-    item.quality should be(1)
+      item.quality should be(1)
+    }
   }
 
 }
