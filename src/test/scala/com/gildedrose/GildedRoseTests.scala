96bcbf80f744bea2b18cbcdf6543d4968aa1a66c
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-04-20 21:52:25 +0200

    Add tests for non-expired regular items

--- src/test/scala/com/gildedrose/GildedRoseTests.scala
+++ src/test/scala/com/gildedrose/GildedRoseTests.scala
@@ -9,9 +9,13 @@ import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 class GildedroseTests extends AnyFunSuite with Matchers with ScalaCheckDrivenPropertyChecks {
 
   val conjuredNameGen: Gen[String] = arbitrary[String].map(name => s"Conjured $name")
-  val qualityGen: Gen[Int]         = Gen.chooseNum(0, 50)
-  val expiredSellInGen: Gen[Int]   = Gen.oneOf(Gen.negNum[Int], Gen.const(0))
-  val validSellInGen: Gen[Int]     = Gen.posNum[Int]
+  val regularNameGen: Gen[String] = arbitrary[String].suchThat {
+    case Item.AgedBrie.name | Item.BackstagePass.name | Item.Sulfuras.name => false
+    case name                                                              => !name.startsWith("Conjured")
+  }
+  val qualityGen: Gen[Int]       = Gen.chooseNum(0, 50)
+  val expiredSellInGen: Gen[Int] = Gen.oneOf(Gen.negNum[Int], Gen.const(0))
+  val validSellInGen: Gen[Int]   = Gen.posNum[Int]
 
   test("Non-expired conjured items should degrade at the rate of 2 every day") {
     forAll(conjuredNameGen -> "name", validSellInGen -> "sellIn", qualityGen -> "quality") { (name, sellIn, quality) =>
@@ -31,6 +35,16 @@ class GildedroseTests extends AnyFunSuite with Matchers with ScalaCheckDrivenPro
         new GildedRose(Array(item)).updateQuality()
 
         item.quality should be(math.max(0, quality - 4))
+    }
+  }
+
+  test("Non-expired regular items should degrade at the rate of 1 every day") {
+    forAll(regularNameGen -> "name", validSellInGen -> "sellIn", qualityGen -> "quality") { (name, sellIn, quality) =>
+      val item = new Item(name, sellIn, quality)
+
+      new GildedRose(Array(item)).updateQuality()
+
+      item.quality should be(math.max(0, quality - 1))
     }
   }
 
