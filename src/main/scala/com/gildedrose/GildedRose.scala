b0def4609275d9a03a78ef24124f8d3544666bc9
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-06-17 23:00:29 +0200

    Extract increase & decrease quality

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -6,35 +6,26 @@ class GildedRose(val items: Array[Item]) {
   def isPass(item: Item)     = item.name.equals("Backstage passes to a TAFKAL80ETC concert")
   def isSulfuras(item: Item) = item.name.equals("Sulfuras, Hand of Ragnaros")
 
+  def increaseQuality(item: Item) =
+    if(item.quality < 50) item.quality = item.quality + 1
+
+  def decreaseQuality(item: Item) =
+    if(item.quality > 0 && !isSulfuras(item))
+      item.quality = item.quality - 1
+
   def updateItemQuality(item: Item): Unit = {
     if(isBrie(item) || isPass(item)) {
-      if(item.quality < 50) {
-        item.quality = item.quality + 1
+      increaseQuality(item)
 
-        if(isPass(item)) {
-          if(item.sellIn < 11) {
-            if(item.quality < 50) {
-              item.quality = item.quality + 1
-            }
-          }
-
-          if(item.sellIn < 6) {
-            if(item.quality < 50) {
-              item.quality = item.quality + 1
-            }
-          }
-        }
+      if(isPass(item)) {
+        if(item.sellIn < 11)
+          increaseQuality(item)
+        if(item.sellIn < 6)
+          increaseQuality(item)
       }
-
     }
-    else {
-      if(item.quality > 0) {
-        if(!isSulfuras(item)) {
-          item.quality = item.quality - 1
-        }
-      }
-
-    }
+    else
+      decreaseQuality(item)
 
     if(!isSulfuras(item)) {
       item.sellIn = item.sellIn - 1
@@ -42,18 +33,13 @@ class GildedRose(val items: Array[Item]) {
 
     if(item.sellIn < 0) {
       if(isBrie(item)) {
-        if(item.quality < 50) {
-          item.quality = item.quality + 1
-        }
+        increaseQuality(item)
       }
       else if(isPass(item)) {
         item.quality = 0
       }
-      else if(item.quality > 0) {
-        if(!isSulfuras(item)) {
-          item.quality = item.quality - 1
-        }
-      }
+      else
+        decreaseQuality(item)
     }
   }
 
