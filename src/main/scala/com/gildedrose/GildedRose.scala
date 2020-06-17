5b51b921c2afe4fd299e76900640de84bcdaf278
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-06-17 22:47:59 +0200

    Make updateQuality into an anonymous function

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -6,58 +6,60 @@ class GildedRose(val items: Array[Item]) {
   def isPass(item: Item)     = item.name.equals("Backstage passes to a TAFKAL80ETC concert")
   def isSulfuras(item: Item) = item.name.equals("Sulfuras, Hand of Ragnaros")
 
-  def updateQuality(): Unit =
-    items.foreach { item =>
-      if(!isBrie(item)
-         && !isPass(item)) {
-        if(item.quality > 0) {
-          if(!isSulfuras(item)) {
-            item.quality = item.quality - 1
+  def updateItemQuality(item: Item): Unit = {
+    if(!isBrie(item)
+       && !isPass(item)) {
+      if(item.quality > 0) {
+        if(!isSulfuras(item)) {
+          item.quality = item.quality - 1
+        }
+      }
+    }
+    else {
+      if(item.quality < 50) {
+        item.quality = item.quality + 1
+
+        if(isPass(item)) {
+          if(item.sellIn < 11) {
+            if(item.quality < 50) {
+              item.quality = item.quality + 1
+            }
           }
+
+          if(item.sellIn < 6) {
+            if(item.quality < 50) {
+              item.quality = item.quality + 1
+            }
+          }
+        }
+      }
+    }
+
+    if(!isSulfuras(item)) {
+      item.sellIn = item.sellIn - 1
+    }
+
+    if(item.sellIn < 0) {
+      if(!isBrie(item)) {
+        if(!isPass(item)) {
+          if(item.quality > 0) {
+            if(!isSulfuras(item)) {
+              item.quality = item.quality - 1
+            }
+          }
+        }
+        else {
+          item.quality = item.quality - item.quality
         }
       }
       else {
         if(item.quality < 50) {
           item.quality = item.quality + 1
-
-          if(isPass(item)) {
-            if(item.sellIn < 11) {
-              if(item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-
-            if(item.sellIn < 6) {
-              if(item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-          }
-        }
-      }
-
-      if(!isSulfuras(item)) {
-        item.sellIn = item.sellIn - 1
-      }
-
-      if(item.sellIn < 0) {
-        if(!isBrie(item)) {
-          if(!isPass(item)) {
-            if(item.quality > 0) {
-              if(!isSulfuras(item)) {
-                item.quality = item.quality - 1
-              }
-            }
-          }
-          else {
-            item.quality = item.quality - item.quality
-          }
-        }
-        else {
-          if(item.quality < 50) {
-            item.quality = item.quality + 1
-          }
         }
       }
     }
+  }
+
+  def updateQuality(): Unit =
+    items.foreach(updateItemQuality)
 }
