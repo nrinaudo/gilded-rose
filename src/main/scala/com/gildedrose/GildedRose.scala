dad99724c79f9d1afb4d3829e503036a1656886d
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-06-17 22:46:27 +0200

    Got rid of for loop

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -2,62 +2,62 @@ package com.gildedrose
 
 class GildedRose(val items: Array[Item]) {
 
-  def isBrie(item: Item) = item.name.equals("Aged Brie")
-  def isPass(item: Item) = item.name.equals("Backstage passes to a TAFKAL80ETC concert")
+  def isBrie(item: Item)     = item.name.equals("Aged Brie")
+  def isPass(item: Item)     = item.name.equals("Backstage passes to a TAFKAL80ETC concert")
+  def isSulfuras(item: Item) = item.name.equals("Sulfuras, Hand of Ragnaros")
 
-  def updateQuality() {
-    for(i <- 0 until items.length) {
-      if(!isBrie(items(i))
-         && !isPass(items(i))) {
-        if(items(i).quality > 0) {
-          if(!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-            items(i).quality = items(i).quality - 1
+  def updateQuality(): Unit =
+    items.foreach { item =>
+      if(!isBrie(item)
+         && !isPass(item)) {
+        if(item.quality > 0) {
+          if(!isSulfuras(item)) {
+            item.quality = item.quality - 1
           }
         }
       }
       else {
-        if(items(i).quality < 50) {
-          items(i).quality = items(i).quality + 1
+        if(item.quality < 50) {
+          item.quality = item.quality + 1
 
-          if(isPass(items(i))) {
-            if(items(i).sellIn < 11) {
-              if(items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
+          if(isPass(item)) {
+            if(item.sellIn < 11) {
+              if(item.quality < 50) {
+                item.quality = item.quality + 1
               }
             }
 
-            if(items(i).sellIn < 6) {
-              if(items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
+            if(item.sellIn < 6) {
+              if(item.quality < 50) {
+                item.quality = item.quality + 1
               }
             }
           }
         }
       }
 
-      if(!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-        items(i).sellIn = items(i).sellIn - 1
+      if(!isSulfuras(item)) {
+        item.sellIn = item.sellIn - 1
       }
 
-      if(items(i).sellIn < 0) {
-        if(!isBrie(items(i))) {
-          if(!isPass(items(i))) {
-            if(items(i).quality > 0) {
-              if(!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-                items(i).quality = items(i).quality - 1
+      if(item.sellIn < 0) {
+        if(!isBrie(item)) {
+          if(!isPass(item)) {
+            if(item.quality > 0) {
+              if(!isSulfuras(item)) {
+                item.quality = item.quality - 1
               }
             }
           }
           else {
-            items(i).quality = items(i).quality - items(i).quality
+            item.quality = item.quality - item.quality
           }
         }
         else {
-          if(items(i).quality < 50) {
-            items(i).quality = items(i).quality + 1
+          if(item.quality < 50) {
+            item.quality = item.quality + 1
           }
         }
       }
     }
-  }
 }
