588bb30be63e74ef99a540af7c8f631e1cd92379
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2020-06-17 23:04:33 +0200

    Make increaseQuality take a quantity parameter

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -6,8 +6,8 @@ class GildedRose(val items: Array[Item]) {
   def isPass(item: Item)     = item.name.equals("Backstage passes to a TAFKAL80ETC concert")
   def isSulfuras(item: Item) = item.name.equals("Sulfuras, Hand of Ragnaros")
 
-  def increaseQuality(item: Item) =
-    if(item.quality < 50) item.quality = item.quality + 1
+  def increaseQuality(item: Item, quantity: Int) =
+    if(item.quality < 50) item.quality = item.quality + quantity
 
   def decreaseQuality(item: Item) =
     if(item.quality > 0 && !isSulfuras(item))
@@ -15,14 +15,17 @@ class GildedRose(val items: Array[Item]) {
 
   def updateItemQuality(item: Item): Unit = {
     if(isBrie(item)) {
-      increaseQuality(item)
+      increaseQuality(item, 1)
     }
     else if(isPass(item)) {
-      increaseQuality(item)
-      if(item.sellIn < 11)
-        increaseQuality(item)
-      if(item.sellIn < 6)
-        increaseQuality(item)
+      if(item.sellIn < 6) {
+        increaseQuality(item, 3)
+      }
+      else if(item.sellIn < 11) {
+        increaseQuality(item, 2)
+      }
+      else
+        increaseQuality(item, 1)
     }
     else
       decreaseQuality(item)
@@ -33,7 +36,7 @@ class GildedRose(val items: Array[Item]) {
 
     if(item.sellIn < 0) {
       if(isBrie(item)) {
-        increaseQuality(item)
+        increaseQuality(item, 1)
       }
       else if(isPass(item)) {
         item.quality = 0
